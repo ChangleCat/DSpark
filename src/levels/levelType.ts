15e9699c67971dcc64aee0568d86f1a78c4e4018
lzewlye <2353787863@qq.com>
2025-04-01 19:41:52 +0800

    start :-)

---
+++ src/levels/levelType.ts
@@ -0,0 +1,8 @@
+export interface Level {
+    id: string;
+    name: string;
+    description: string;
+    premises: string[]; // 公式暂时还是字符串
+    goal: string;       // 公式暂时还是字符串
+    availableRules?: string[];
+  }
