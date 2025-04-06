e0b42e0363abff3430013e7c6b7aac4d6cc7e073
Tiamd3 <15096959727@163.com>
2025-04-06 20:49:26 +0800

    add ast

---
+++ src/levels/rule.ts
@@ -0,0 +1,16 @@
+export interface BaseRule {// 基本规则接口
+    id: number; // 规则的唯一标识符
+    name: string; // 规则的名称
+    formula: string; // 规则的公式
+    command: string; // 规则的命令
+    description: string; // 规则的描述
+}
+
+export interface Theory {// 规则接口
+    id: number; // 规则的唯一标识符
+    name: string; // 规则的名称
+    formula: string; // 规则的公式
+    command: string; // 规则的命令
+    description: string; // 规则的描述
+}
+
